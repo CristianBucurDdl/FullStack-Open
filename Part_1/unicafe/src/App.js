db32c5f68a682a53f3579c077e51056d4e3ceb33
Cristian Bucur <cristian.bucur@dedalus.eu>
2023-06-13 13:19:47 +0100

    unicafe step 3

--- Part_1/unicafe/src/App.js
+++ Part_1/unicafe/src/App.js
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.all}</p>
+      <p>average{props.averageTotal}</p>
+      <p>positive {props.positive}</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -9,16 +23,24 @@ const App = () => {
   const Button = (props) => (
     <button onClick={props.handleClick}>{props.text}</button>
   );
+  function percentage(partialValue, totalValue) {
+    return (100 * partialValue) / totalValue;
+  }
+  const porps = {
+    all: good + neutral + bad,
+    average1: good - bad,
+    averageTotal: average1 / all,
+
+    positive: percentage(good, all),
+  };
+
   return (
     <div>
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text=" bad" />
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics props={porps} />
     </div>
   );
 };
