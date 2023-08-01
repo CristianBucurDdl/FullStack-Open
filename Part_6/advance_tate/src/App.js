b183387ca737c332fa7606f37d42ef6f707d71da
Cristian Bucur <cristian.bucur@dedalus.eu>
2023-08-01 15:47:31 +0100

    counterApp_with_store

---
+++ Part_6/advance_tate/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { createStore } from "redux";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return (state = 0);
+    default:
+      return state;
+  }
+};
+const store = createStore(counterReducer);
+
+function App() {
+  return (
+    <div className="App">
+      <h1>counter</h1>
+      <div>{store.getState()}</div>
+      <button onClick={(e) => store.dispatch({ type: "INCREMENT" })}>
+        INCREMENT
+      </button>
+      <button onClick={(e) => store.dispatch({ type: "DECREMENT" })}>
+        Decrement
+      </button>
+      <button onClick={(e) => store.dispatch({ type: "ZERO" })}>Zero</button>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+// renderApp();
+store.subscribe(renderApp);
+export default App;
