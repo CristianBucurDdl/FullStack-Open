6083c2bb42aafd752734772fda1305f04b024aae
Cristian Bucur <cristian.bucur@dedalus.eu>
2023-08-24 15:09:14 +0100

    added store in its own file

--- Part_6/redux-notes/src/index.js
+++ Part_6/redux-notes/src/index.js
@@ -1,20 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import App from "./App";
-import noteService from "./services/notes";
-import noteReducer, { appendNote, setNotes } from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-// noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
+import { store } from "./store";
 
 console.log(store.getState());
 ReactDOM.createRoot(document.getElementById("root")).render(
