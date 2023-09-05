4383963dde95ead88586645a088813b65ae8397d
Cristian Bucur <cristian.bucur@dedalus.eu>
2023-09-05 13:24:19 +0100

    exercices up til 6.21

--- Part_6/redux-notes3.0/src/App.jsx
+++ Part_6/redux-notes3.0/src/App.jsx
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
+
 import { createNote, getNotes, updateNote } from "./requests";
 const App = () => {
   const queryClient = useQueryClient();
