3d58eadaf29d735675db1b2411196a228c809d8c
Cristian Bucur <cristian.bucur@dedalus.eu>
2023-11-27 16:55:18 +0000

    ex 3.5

--- Part_3/exercices/3.1-3.6/index.js
+++ Part_3/exercices/3.1-3.6/index.js
@@ -23,9 +23,28 @@ const phoneBook = [
     number: "39-23-6423122",
   },
 ];
+const currentDate = new Date().toLocaleString();
+
+// const infoContent = (
+//   <p>{`Phonebook has info for ${phoneBook.length} people `}</p>
+// );
 
 app.get("/api/persons", (request, response) => {
   response.json(phoneBook);
+});
+
+app.get("/info", (request, response) => {
+  response.writeHead(200, { "Content-type": "html" });
+  response.end(
+    `<p>Phonebook has info for ${phoneBook.length} people</p><br/> <p>${currentDate}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const phoneNumber = phoneBook.find((phone) => phone.id === id);
+
+  phoneNumber ? response.json(phoneNumber) : response.status(400).end();
 });
 
 const PORT = 3001;
