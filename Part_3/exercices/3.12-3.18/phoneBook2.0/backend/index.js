7e3b9a973e80d286976ac1b47c03b01d282c313a
Cristian Bucur <cristian.bucur@dedalus.eu>
2024-01-18 15:11:28 +0000

    fixing issue to deply failiure

--- Part_3/exercices/3.12-3.18/phoneBook2.0/backend/index.js
+++ Part_3/exercices/3.12-3.18/phoneBook2.0/backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("dist"));
+// app.use(express.static("dist"));
 const phoneBook = [
   {
     id: 1,
